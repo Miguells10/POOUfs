8b8502b1293692d8d667f2d45caf21bd97ece5a9
Miguel Lucas <lucasmiguel646@gmail.com>
2024-07-23 22:21:53 -0300

    Alterei a forma como o objeto era instanciado, troquei de listas para vetores

--- Atividades/src/Atv1/Program.java
+++ Atividades/src/Atv1/Program.java
@@ -1,34 +1,23 @@
 package Atv1;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Program {
     public static void main(String[] args) {
 
-        List<Livro> lista = new ArrayList<>();
+        Livro[] livros = new Livro[5]; //Criação de um array de livros
 
-        Livro l1 = new Livro("Livro 1", "Autor 1", "Editora 1", 100, "Estilo 1", 50.0, 2021);
-        Livro l2 = new Livro("Livro 2", "Autor 2", "Editora 2", 200, "Estilo 2", 100.0, 2020);
-        Livro l3 = new Livro("Livro 3", "Autor 3", "Editora 3", 300, "Estilo 3", 150.0, 2019);
-        Livro l4 = new Livro("Livro 4", "Autor 4", "Editora 4", 400, "Estilo 4", 200.0, 2018);
-        Livro l5 = new Livro("Livro 5", "Autor 5", "Editora 5", 500, "Estilo 5", 250.0, 2017);
+        livros[0] = new Livro("Uncle Fish", "Paranoid", "Atibaia", 100, "Ação", 100.0, 2021);
+        livros[1] = new Livro("Livro 2", "Autor 2", "Editora 2", 200, "Estilo 2", 20.0, 2020);
+        livros[2] = new Livro("Livro 3", "Autor 3", "Editora 3", 300, "Estilo 3", 30.0, 2019);
+        livros[3] = new Livro("Livro 4", "Autor 4", "Editora 4", 400, "Estilo 4", 40.0, 2018);
+        livros[4] = new Livro("Livro 5", "Autor 5", "Editora 5", 500, "Estilo 5", 50.0, 2017);
 
-        lista.add(l1);
-        lista.add(l2);
-        lista.add(l3);
-        lista.add(l4);
-        lista.add(l5);
-
-
-        System.out.println("Lista de livros:");
-        for(Livro livro : lista){
-            livro.mostrarLivro();
-            livro.reajusteValor();
+        for (Livro livro : livros) {
+            livro.mostrarLivro(); //Irá exibir os dados dos livros percorrendo o array
+            livro.reajusteValor(); //Irá reajustar o valor dos livros percorrendo o array
         }
 
-        System.out.println("Lista de livros após o reajuste:");
-        for(Livro livro : lista){
+        System.out.println("\nApós reajuste de valor:\n");
+
+        for (Livro livro : livros) { //Irá exibir novamente os dados dos livros percorrendo o array
             livro.mostrarLivro();
         }
 
